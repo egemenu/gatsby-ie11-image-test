0b0c0c5f76d52553d554ae42e8440ec1265a1b2e
Egemen <egemen.unuvar@esseninternational.com>
2021-05-08 16:07:14 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import { StaticImage, GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,9 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Test</h1>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
